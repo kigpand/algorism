58ab0e2cf9d7a47a42a64333255115574994aaa4
kigpand <kigpand@naver.com>
2022-07-17 12:07:41 +0900

    부족한금액계산하기

---
+++ 부족한금액계산하기/main.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    let total = 0;
+    for(let i=1; i<=count; i++) total += price * i;
+
+    if (total - money > 0) return total - money;
+    return 0;
+}
+
+console.log(solution(3, 20, 4));
