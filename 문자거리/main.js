9bea22d4b8f2c98f0c8bee87decb027612d87a61
김지훈 <kigpand@gmail.com>
2024-04-29 23:11:31 +0900

    영상 풀이

--- 문자거리/main.js
+++ 문자거리/main.js
@@ -1,26 +1,30 @@
 // 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
 function solution(s, t) {
-  const arr = s.split("");
+  let answer = [];
+  let p = 1000;
 
-  // 첫번째 t값 위치
-  let targetIndex = arr.indexOf(t);
-
-  const result = arr.map((item, index) => {
-    // 현재 index가 t값보다 작을경우
-    if (index <= targetIndex) return targetIndex - index;
-
-    // 현재 index가 클 경우 다음 t값 위치 변수
-    const nextTargetIndex = arr.indexOf(t, targetIndex + 1);
-
-    // 현재 위치가 다음 t값 위치보다 멀 경우 t값 위치 수정
-    if (Math.abs(nextTargetIndex - index) <= Math.abs(targetIndex - index)) {
-      targetIndex = nextTargetIndex;
-      return Math.abs(nextTargetIndex - index);
+  for (let x of s) {
+    if (x === t) {
+      p = 0;
+      answer.push(p);
+    } else {
+      p++;
+      answer.push(p);
     }
-    return Math.abs(targetIndex - index);
-  });
+  }
+  console.log(answer);
+  p === 1000;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) {
+      p = 0;
+      answer[i] === p;
+    } else {
+      p++;
+      answer[i] = Math.min(answer[i], p);
+    }
+  }
 
-  return result;
+  return answer;
 }
 
 console.log(solution("teachermode", "e"));
