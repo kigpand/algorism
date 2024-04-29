a71bc5eea22cbef4edbbef6151e6604efc99ea25
김지훈 <kigpand@gmail.com>
2024-04-29 23:02:15 +0900

    내 정답 풀이

--- 문자거리/main.js
+++ 문자거리/main.js
@@ -1,22 +1,19 @@
 // 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
 function solution(s, t) {
   const arr = s.split("");
-  let tNum = arr.findIndex((item) => item === t);
-  const result = arr.map((item, i) => {
-    if (i === tNum) return 0;
-    if (i <= tNum) return tNum - i;
-    if (i >= tNum) {
-      const newArr = arr.slice(i, arr.length);
-      const newNum = newArr.findIndex((item) => item === t);
-      if (newNum - i < tNum - i) {
-        tNum = newNum;
-        return newNum - i;
-      } else {
-        return tNum - i;
-      }
+
+  let targetIndex = arr.indexOf(t);
+  const result = arr.map((item, index) => {
+    if (index <= targetIndex) return targetIndex - index;
+    const nextTargetIndex = arr.indexOf(t, targetIndex + 1);
+    if (Math.abs(nextTargetIndex - index) <= Math.abs(targetIndex - index)) {
+      targetIndex = nextTargetIndex;
+      return Math.abs(nextTargetIndex - index);
     }
+    return Math.abs(targetIndex - index);
   });
-  console.log(result);
+
+  return result;
 }
 
-solution("teachermode", "e");
+console.log(solution("teachermode", "e"));
